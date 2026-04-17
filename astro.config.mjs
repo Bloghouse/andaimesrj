5e43d3259fd1caa4cf3fe436e5a9952f10cfa683
Bloghouse <bloghouse021@gmail.com>
2026-04-16 21:17:02 -0300

    fix: remove adaptador vercel para garantir estabilidade do sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://andaimesrj.com.br',
-  output: 'hybrid',
-  adapter: vercel(),
   integrations: [tailwind(), sitemap()],
 });
